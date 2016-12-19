32cc07c7607ee08e78e97ae5f965573e515e2971
danidaryaweesh <dani_kurkees@hotmail.com>
2016-12-19 14:47:14 +0100

    angular fully working!

--- app/view1/controller.js
+++ app/view1/controller.js
@@ -5,108 +5,48 @@
 var app = angular.module('myApp',[]);
 var login = false;
 
-/*
-app.controller('loginController', function($scope, socket, $location) {
-
-    $scope.username="";
-    $scope.password="";
-    $scope.test="woho!";
-
-    // var taskSocket = new WebSocket("ws://localhost:1337");
-
-    $scope.login = function () {
-    //    if(login){
-    //        console.log("You are already logged in");
-    //    }else{
-            // fixa logg in grejerna
-            /*   var loginMesssage = JSON.stringify({username : $scope.username, password: $scope.password});
-             task.send(loginMesssage);
-             console.log("Logg in succesfull").then(function (data) {
-             if(data == true){
-             console.log("Logged in successfully!");
-             login = true;
-             $window.location.href = '/chat.html';
-             }else{
-             console.log("Coldnt log in!");
-             }
-             }); */
-        //    socket.addSocket("ws://localhost:1337");
-
-        //    window.location.href = "chat.html";
-    //    }
-//    };
-// });
-
-/*
-app.service('socket', function($window) {
-
-    var key='object';
-
-    var addSocket = function (newObj) {
-        var objectData = $window.sessionStorage.getItem(key);
-        if(objectData){
-            objectData = JSON.parse(objectData);
-        }else{
-            objectData = [];
-        }
-        var web = new WebSocket(newObj);
-        objectData.push(web);
-        $window.sessionStorage.setItem(key, JSON.stringify(objectData));
-    };
-
-    var getSocket = function () {
-        var objectData = $window.sessionStorage.getItem(key);
-        if(objectData){
-            objectData =  JSON.parse(objectData);
-        }
-        return objectData || [];
-    };
-
-    return{
-        addSocket: addSocket,
-        getSocket: getSocket
-    };
-}); */
-
 app.controller('personCtrl', function($scope) {
 
+    $scope.username=getUrlParameter('username');
+    $scope.password="";
     $scope.to="";
     $scope.messageText="";
     $scope.message="begining";
-    $scope.id=Math.floor((Math.random() * 10000000) + 1).toString();
-
-    var taskSocket = new WebSocket("ws://localhost:1337");
+    $scope.id = Math.floor((Math.random() * 10000000) + 1).toString();
 
 
-   taskSocket.onmessage = function(message) {
-       var wholeMessage = JSON.parse(message.data);
+    var taskSocket = new WebSocket("ws://130.229.183.91:1337");
 
-       console.log("In else");
-       var from = wholeMessage.from;
-       var body = wholeMessage.body;
-       $scope.message = from+":"+body;
-       console.log("in else from: "+from + " ,body: "+body +" ,$scope.to: "+$scope.to);
-       var para = document.createElement("P");
-       var t = document.createTextNode($scope.message);
-       para.appendChild(t);
-       document.getElementById("chatbox").appendChild(para);
-       $scope.$apply();
-       $scope.message = "";
+
+
+    taskSocket.onmessage = function(message) {
+        if(message.data.indexOf("/wrong password") >=0){
+            console.log("redirecting!");
+           window.location = "/chat.html";
+        }else {
+            $scope.message = message.data;
+            var para = document.createElement("P");
+            var t = document.createTextNode($scope.message);
+            para.appendChild(t);
+            document.getElementById("chatbox").appendChild(para);
+            $scope.$apply();
+        }
     };
 
     taskSocket.onclose = function() {
-
         $scope.message = {
             type: "danger",
             short: "Socket error",
             long: "An e startrror occured with the WebSocket."
     };
+
+        $scope.username="SOCKET CLOSED!";
         $scope.$apply();
     };
 
     $scope.send = function () {
         var body=$scope.messageText;
-        var messageToSend = JSON.stringify({from : $scope.username, body: body, to: $scope.to, id: $scope.id});
+        var messageToSend = JSON.stringify({from : $scope.username, body: body, to: $scope.to, id: $scope.id, password: $scope.password, option:""});
         taskSocket.send(messageToSend);
 
         var para = document.createElement("P");
@@ -133,44 +73,71 @@ app.controller('personCtrl', function($scope) {
         else
             button.style.display = 'block';
 
-        var messageToSend = JSON.stringify({name : $scope.username, to: $scope.to, id: $scope.id});
+        var messageToSend = JSON.stringify({name : $scope.username, to: $scope.to, id: $scope.id, password: $scope.password, option:"connect"});
         taskSocket.send(messageToSend);
+
     };
+
+    $scope.register = function () {
+        var registerMessage = JSON.stringify({username: $scope.username, password: $scope.password, option: "register"});
+        taskSocket.send(registerMessage);
+
+        var form = document.getElementById('info');
+        if (form.style.display === 'block' || form.style.display === '')
+            form.style.display = 'none';
+        else
+            form.style.display = 'block';
+
+        var form2 = document.getElementById('wrapper');
+        if (form2.style.display === 'block' || form2.style.display === '')
+            form2.style.display = 'none';
+        else
+            form2.style.display = 'block';
+
+        var form3 = document.getElementById('loginForm');
+        if (form3.style.display === 'block' || form3.style.display === '')
+            form3.style.display = 'none';
+        else
+            form3.style.display = 'block';
+
+    };
+
+    $scope.login = function () {
+        var loginMessage = JSON.stringify({username: $scope.username, password: $scope.password, id: $scope.id ,option: "login"});
+        taskSocket.send(loginMessage);
+
+        var form = document.getElementById('info');
+        if (form.style.display === 'block' || form.style.display === '')
+            form.style.display = 'none';
+        else
+            form.style.display = 'block';
+
+        var form2 = document.getElementById('wrapper');
+        if (form2.style.display === 'block' || form2.style.display === '')
+            form2.style.display = 'none';
+        else
+            form2.style.display = 'block';
+
+        var form3 = document.getElementById('loginForm');
+        if (form3.style.display === 'block' || form3.style.display === '')
+            form3.style.display = 'none';
+        else
+            form3.style.display = 'block';
+    };
+
+    function getUrlParameter(param, dummyPath) {
+        var sPageURL = dummyPath || window.location.search.substring(1),
+            sURLVariables = sPageURL.split(/[&||?]/),
+            res;
+
+        for (var i = 0; i < sURLVariables.length; i += 1) {
+            var paramName = sURLVariables[i],
+                sParameterName = (paramName || '').split('=');
+
+            if (sParameterName[0] === param) {
+                res = sParameterName[1];
+            }
+        }
+        return res;
+    }
 });
-
-
-/*
- <!DOCTYPE html>
- <html lang="en">
- <head>
- <meta charset="UTF-8">
- <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
- <script src="bower_components/angular-websocket/dist/angular-websocket.js"></script>
- <script src="view1/controller.js"></script>
- <title>Home Page</title>
- </head>
-
- <body>
-
- <div ng-app="myApp" ng-controller="personCtrl">
-
-
- Message: <input type="text" ng-model="messageText">
- <br /><br />
- <button ng-click='send()'>Send Message</button>
- <br /> <br /> <br />
- Message: {{task}}
-
- <div>
-
-
-
- </div>
-
-
- </div>
-
- </body>
-
- </html>
- */
